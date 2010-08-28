13f6d2fef1b5cb49b3eb18a213b02dfffcdff3e1
Michael Bleigh <michael@intridea.com>
2010-08-27 22:30:45 -0500

    Updating the look.

--- server.coffee
+++ server.coffee
@@ -4,11 +4,12 @@ require 'express'
 require 'oauth'
 
 sys = require 'sys'
-oauth = require 'oauth'
+OAuth = require('oauth').OAuth
 url = require 'url'
 connect = require 'connect'
 express = require 'express'
 ejs = require 'ejs'
+Twitter = new OAuth('http://api.twitter.com/oauth/request_token', 'http://api.twitter.com/oauth/access_token', process.env.TWITTER_KEY, process.env.TWITTER_SECRET, '1.0', null, 'HMAC-SHA1')
 
 # Setup Hoptoad Notification
 hoptoad = require('hoptoad-notifier').Hoptoad
@@ -21,5 +22,16 @@ app.set 'view engine', 'ejs'
 
 app.get '/', (req,res)->
   res.render 'home.ejs'
+  
+app.get '/sign_in', (req, res)->
+  Twitter.getOAuthRequestToken (error, token, secret, url, params)->
+    req.session['req.token'] = token
+    req.session['req.secret'] = secret
+    res.redirect "http://api.twitter.com/oauth/authenticate?oauth_token=#{token}"
 
+app.get '/oauth/callback', (req, res)->
+  Twitter.getOAuthAccessToken req.session['req.token'], req.session['req.secret'], (error, access_token, access_secret, params)->
+    Twitter.getProtectedResource 'http://api.twitter.com/1/account/verify_credentials.json', 'GET', access_token, access_secret, (error, data, response)->
+      res.render data
+    
 app.listen parseInt(process.env.PORT) || 3000
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 (function() {
-  var app, connect, ejs, express, hoptoad, oauth, sys, url;
+  var OAuth, Twitter, app, connect, ejs, express, hoptoad, sys, url;
   require.paths.unshift('./vendor');
   require('express');
   require('oauth');
   sys = require('sys');
-  oauth = require('oauth');
+  OAuth = require('oauth').OAuth;
   url = require('url');
   connect = require('connect');
   express = require('express');
   ejs = require('ejs');
+  Twitter = new OAuth('http://api.twitter.com/oauth/request_token', 'http://api.twitter.com/oauth/access_token', process.env.TWITTER_KEY, process.env.TWITTER_SECRET, '1.0', null, 'HMAC-SHA1');
   hoptoad = require('hoptoad-notifier').Hoptoad;
   hoptoad.key = '63da924b138bae57d1066c46accddbe7';
   process.addListener('uncaughtException', function(error) {
@@ -18,6 +19,20 @@
   app.set('view engine', 'ejs');
   app.get('/', function(req, res) {
     return res.render('home.ejs');
+  });
+  app.get('/sign_in', function(req, res) {
+    return Twitter.getOAuthRequestToken(function(error, token, secret, url, params) {
+      req.session['req.token'] = token;
+      req.session['req.secret'] = secret;
+      return res.redirect(("http://api.twitter.com/oauth/authenticate?oauth_token=" + (token)));
+    });
+  });
+  app.get('/oauth/callback', function(req, res) {
+    return Twitter.getOAuthAccessToken(req.session['req.token'], req.session['req.secret'], function(error, access_token, access_secret, params) {
+      return Twitter.getProtectedResource('http://api.twitter.com/1/account/verify_credentials.json', 'GET', access_token, access_secret, function(error, data, response) {
+        return res.render(data);
+      });
+    });
   });
   app.listen(parseInt(process.env.PORT) || 3000);
 })();
