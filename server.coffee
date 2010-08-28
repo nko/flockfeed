2bc2744d7f03650d43641eb5e7cd299a4437cd38
Michael Bleigh <michael@intridea.com>
2010-08-27 19:58:40 -0500

    Add Hoptoad for Exception notification.

--- server.coffee
+++ server.coffee
@@ -10,6 +10,12 @@ connect = require 'connect'
 express = require 'express'
 ejs = require 'ejs'
 
+# Setup Hoptoad Notification
+hoptoad = require('hoptoad-notifier').Hoptoad
+hoptoad.key = '63da924b138bae57d1066c46accddbe7'
+process.addListener 'uncaughtException', (error)->
+  hoptoad.notify(error)
+
 app = express.createServer connect.cookieDecoder(), connect.session()
 
 app.get '/', (req,res)->
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, connect, ejs, express, oauth, sys, url;
+  var app, connect, ejs, express, hoptoad, oauth, sys, url;
   require.paths.unshift('./vendor');
   require('express');
   require('oauth');
@@ -9,6 +9,11 @@
   connect = require('connect');
   express = require('express');
   ejs = require('ejs');
+  hoptoad = require('hoptoad-notifier').Hoptoad;
+  hoptoad.key = '63da924b138bae57d1066c46accddbe7';
+  process.addListener('uncaughtException', function(error) {
+    return hoptoad.notify(error);
+  });
   app = express.createServer(connect.cookieDecoder(), connect.session());
   app.get('/', function(req, res) {
     return res.send("Hello from Express!");
